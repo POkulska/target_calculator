7509cef1a53b5bdf27e046e9bcf1943a31fcde27
POkulska <paulina.okulska@gmail.com>
2020-02-16 18:52:20 +0100

    emptythinggoingon

--- src/App.js
+++ src/App.js
@@ -24,10 +24,21 @@ function App() {
         setVideoNum(videoNum-1)
       }
     } else if (parent === 'comment') {
-      (job === 'plus') ? setCommentNum(commentNum+1) : setCommentNum(commentNum-1)
+      if (job === 'plus') {
+        setCommentNum(commentNum+1)
+      } else if(commentNum !== 0) {
+        setCommentNum(commentNum-1)
+      }
+    
     } else {
-      (job === 'plus') ? setLiveSNum(liveSNum+1) : setLiveSNum(liveSNum-1)
+      if (job === 'plus') {
+        setLiveSNum(liveSNum+1)
+      } else if(liveSNum !== 0) {
+        setLiveSNum(liveSNum-1)
+      }
     }
+
+
   }
 
   const changeHandler = e => {
@@ -46,6 +57,18 @@ function App() {
         <span>Video</span>
         <span className="button" onClick={clickHandler} data-parent="video" data-job="plus">+</span>
         {' : '}{videoNum}
+      </div>
+      <div id="comment">
+        <span className="button" onClick={clickHandler} data-parent="comment" data-job="minus">-</span>
+        <span>Comment</span>
+        <span className="button" onClick={clickHandler} data-parent="comment" data-job="plus">+</span>
+        {' : '}{commentNum}
+      </div>
+      <div id="livestream">
+        <span className="button" onClick={clickHandler} data-parent="livestream" data-job="minus">-</span>
+        <span>Live Stream</span>
+        <span className="button" onClick={clickHandler} data-parent="livestream" data-job="plus">+</span>
+        {' : '}{liveSNum}
       </div>
       <Form changeHandler={changeHandler}/>
       <TargetView commentT={commentT} videoT={videoT}/>
---
+++ src/Components/Comment/Comment.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Comment = () => <div>COMMENT</div>
+
+export default Comment
